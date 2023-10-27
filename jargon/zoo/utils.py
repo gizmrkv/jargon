60891bc0acfab62021bd7f8027a679308ec18d19
gizmrkv <gizmrkv@gmail.com>
2023-10-27 23:15:57 +0900

    Add zoo.utils and wandb_sweep

---
+++ jargon/zoo/utils.py
@@ -0,0 +1,24 @@
+import datetime
+from typing import Any, Callable, Dict
+
+import wandb
+from jargon.utils import WandbLogger
+
+
+def wandb_sweep(
+    config: Dict[str, Any],
+    main: Callable[..., Any],
+    sweep_id: str | None = None,
+    project: str = "jargon",
+    prefix: str = "",
+) -> None:
+    if sweep_id is None:
+        sweep_id = wandb.sweep(sweep=config, project=project)
+
+    def func() -> None:
+        dt = datetime.datetime.now()
+        name = dt.strftime("%Y/%m/%d %H:%M:%S.%f")
+        logger = WandbLogger(prefix=prefix, name=name)
+        main(logger=logger, **wandb.config)
+
+    wandb.agent(sweep_id, func, project=project)
