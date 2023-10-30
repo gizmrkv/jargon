0a0dcccd9b744262fbe851a22c2d528f07750e65
gizmrkv <gizmrkv@gmail.com>
2023-10-30 22:30:01 +0900

    Update pad_sequence module

--- jargon/net/functional/__init__.py
+++ jargon/net/functional/__init__.py
@@ -1,1 +1,4 @@
 from .discounted_cumulative_reward import discounted_cumulative_reward
+from .padding_mask import padding_mask
+
+__all__ = ["discounted_cumulative_reward", "padding_mask"]
---
+++ jargon/net/functional/padding_mask.py
@@ -0,0 +1,32 @@
+import torch
+from torch import Tensor
+
+
+def padding_mask(message: Tensor) -> Tensor:
+    """Create a mask for the padding in the message.
+
+    Parameters
+    ----------
+    message : Tensor
+        The message to create the mask for.
+
+    Returns
+    -------
+    Tensor
+        The mask for the padding in the message.
+        0 is placed at the position to pad, and 1 is placed at the other positions.
+
+    Examples
+    --------
+    >>> message = torch.tensor([[1, 2, 3, 0, 0], [1, 2, 0, 0, 0]])
+    >>> padding_mask(message)
+    tensor([[1, 1, 1, 1, 0],
+            [1, 1, 1, 0, 0]])
+    """
+    mask = message == 0
+    indices = torch.argmax(mask.int(), dim=1)
+    no_mask = ~mask.any(dim=1)
+    indices[no_mask] = message.shape[1]
+    mask = torch.arange(message.shape[1]).expand(message.shape).to(message.device)
+    mask = (mask <= indices.unsqueeze(-1)).long()
+    return mask
