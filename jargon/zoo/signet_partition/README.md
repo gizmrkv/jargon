9720ed0def148819aaec3368c6379a42d2f39a10
gizmrkv <gizmrkv@gmail.com>
2023-11-28 20:50:43 +0900

    Add signet_partition

---
+++ jargon/zoo/signet_partition/README.md
@@ -0,0 +1,1 @@
+https://www.semanticscholar.org/paper/R-EVISITING-P-OPULATIONS-IN-M-ULTI-A-GENT-C-Michel-Mathewson/cecf4bb989eee5e208869c079701205bb347d05c
---
+++ jargon/zoo/signet_partition/train_partition.py
@@ -0,0 +1,140 @@
+from copy import deepcopy
+from typing import Any, Dict, List, Literal, Type
+
+from torch import nn
+
+from jargon.game import SignalingNetworkGame
+from jargon.net import MLP, MultiDiscreteMLP, Receiver, Sender
+from jargon.zoo.signet.loss import Loss
+from jargon.zoo.signet.train import train
+
+
+def train_partition(
+    num_elems: int = 100,
+    num_attrs: int = 2,
+    vocab_size: int = 100,
+    max_len: int = 3,
+    entropy_loss_weight: float = 0.5,
+    length_loss_weight: float = 0.0,
+    num_agents: int = 2,
+    network_mode: Literal["fully"] = "fully",
+    encoder_embedding_dim: int = 8,
+    encoder_hidden_sizes: List[int] = [64],
+    encoder_activation_type: Type[nn.Module] | str = nn.GELU,
+    encoder_activation_args: Dict[str, Any] | None = None,
+    encoder_normalization_type: Type[nn.Module] | str | None = nn.LayerNorm,
+    encoder_normalization_args: Dict[str, Any] | None = None,
+    encoder_dropout: float = 0.0,
+    sender_input_dim: int = 64,
+    sender_embedding_dim: int = 8,
+    sender_hidden_size: int = 500,
+    sender_num_layers: int = 1,
+    sender_cell_type: Type[nn.Module] | str = nn.GRU,
+    sender_cell_args: Dict[str, Any] | None = None,
+    decoder_hidden_sizes: List[int] = [64],
+    decoder_activation_type: Type[nn.Module] | str = nn.GELU,
+    decoder_activation_args: Dict[str, Any] | None = None,
+    decoder_normalization_type: Type[nn.Module] | str | None = nn.LayerNorm,
+    decoder_normalization_args: Dict[str, Any] | None = None,
+    decoder_dropout: float = 0.0,
+    receiver_output_dim: int = 64,
+    receiver_embedding_dim: int = 8,
+    receiver_hidden_size: int = 500,
+    receiver_num_layers: int = 1,
+    receiver_cell_type: Type[nn.Module] | str = nn.GRU,
+    receiver_cell_args: Dict[str, Any] | None = None,
+    **train_args: Any,
+) -> None:
+    encoder = MultiDiscreteMLP(
+        high=num_elems,
+        n=num_attrs,
+        output_dim=sender_input_dim,
+        embedding_dim=encoder_embedding_dim,
+        hidden_sizes=encoder_hidden_sizes,
+        activation_type=encoder_activation_type,
+        activation_args=encoder_activation_args,
+        normalization_type=encoder_normalization_type,
+        normalization_args=encoder_normalization_args,
+        dropout=encoder_dropout,
+    )
+    sender = Sender(
+        encoder=encoder,
+        input_dim=sender_input_dim,
+        vocab_size=vocab_size,
+        length=max_len,
+        embedding_dim=sender_embedding_dim,
+        hidden_size=sender_hidden_size,
+        num_layers=sender_num_layers,
+        cell_type=sender_cell_type,
+        cell_args=sender_cell_args,
+    )
+    decoder = MLP(
+        input_dim=receiver_output_dim,
+        output_dim=num_elems * num_attrs,
+        hidden_sizes=decoder_hidden_sizes,
+        activation_type=decoder_activation_type,
+        activation_args=decoder_activation_args,
+        normalization_type=decoder_normalization_type,
+        normalization_args=decoder_normalization_args,
+        dropout=decoder_dropout,
+    )
+    receiver = Receiver(
+        decoder=decoder,
+        vocab_size=vocab_size,
+        output_dim=receiver_output_dim,
+        num_elems=num_elems,
+        num_attrs=num_attrs,
+        embedding_dim=receiver_embedding_dim,
+        hidden_size=receiver_hidden_size,
+        num_layers=receiver_num_layers,
+        cell_type=receiver_cell_type,
+        cell_args=receiver_cell_args,
+    )
+
+    senders = {f"S{i}": deepcopy(sender) for i in range(num_agents)}
+    receivers = {f"R{i}": deepcopy(receiver) for i in range(num_agents)}
+
+    if network_mode == "fully":
+        network = {s: {r for r in receivers} for s in senders}
+    else:
+        raise ValueError(f"Unknown graph mode: {network_mode}")
+
+    adaptation_targets = {s: {r for r in receivers} for s in senders}
+    adaptation_targets |= {f"R{i}": {f"S{i}"} for i in range(num_agents)}
+
+    game = SignalingNetworkGame(senders, receivers, network)
+    loss = Loss(
+        num_elems=num_elems,
+        num_attrs=num_attrs,
+        vocab_size=vocab_size,
+        max_len=max_len,
+        game=game,
+        adaptation_targets=adaptation_targets,
+        entropy_loss_weight=entropy_loss_weight,
+        length_loss_weight=length_loss_weight,
+    )
+
+    train(
+        num_elems=num_elems,
+        num_attrs=num_attrs,
+        vocab_size=vocab_size,
+        max_len=max_len,
+        game=game,
+        loss_fn=loss,
+        additional_metrics_fn=loss.metrics,
+        **train_args,
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    from jargon.zoo.utils import read_config, wandb_sweep
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--conf_path", "-c", type=str, default=None)
+    parser.add_argument("--sweep_id", "-s", type=str, default=None)
+    args = parser.parse_args()
+
+    conf = read_config(args.conf_path) if args.conf_path else None
+    wandb_sweep(train_partition, conf, args.sweep_id)
