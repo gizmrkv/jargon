3d2ea45d2706a30978952723ae9f4469ce3c60dc
gizmrkv <gizmrkv@gmail.com>
2023-12-04 17:28:04 +0900

    Delete Sender and Receiver

--- jargon/net/__init__.py
+++ jargon/net/__init__.py
@@ -1,7 +1,7 @@
 from .discrete_receiver import DiscreteReceiver
 from .discrete_sender import DiscreteSender
 from .loss import PGLoss
-from .mlp import MLP, MultiDiscreteMLP
+from .mlp import MLP
 from .rnn import RNN
 
 __all__ = [
