d57b74035f9dddc47a18af5779a79e109cfdcc36
gizmrkv <gizmrkv@gmail.com>
2023-12-08 01:37:17 +0900

    Remove Sampler module from functional package

--- jargon/net/functional/__init__.py
+++ jargon/net/functional/__init__.py
@@ -1,6 +1,5 @@
 from .discounted_cumulative_reward import discounted_cumulative_reward
 from .drop_padding import drop_padding
 from .padding_mask import padding_mask
-from .sampler import Sampler
 
-__all__ = ["discounted_cumulative_reward", "padding_mask", "Sampler", "drop_padding"]
+__all__ = ["discounted_cumulative_reward", "padding_mask", "drop_padding"]
