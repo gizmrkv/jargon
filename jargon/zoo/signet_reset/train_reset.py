c9264c369b72a4cb163ae74cdc8a3766b00c0046
gizmrkv <gizmrkv@gmail.com>
2023-12-08 16:49:10 +0900

    Add reset_num_senders_once and reset_num_receivers_once parameters

--- jargon/zoo/signet_reset/train_reset.py
+++ jargon/zoo/signet_reset/train_reset.py
@@ -9,7 +9,9 @@ from jargon.zoo.signet.train import train
 def train_reset(
     game: SignalingNetworkGame,
     reset_senders_interval: int | None = None,
+    reset_num_senders_once: int = 1,
     reset_receivers_interval: int | None = None,
+    reset_num_receivers_once: int = 1,
     **train_args: Any,
 ) -> None:
     assert (
@@ -23,15 +25,15 @@ def train_reset(
     receivers = list(game.receivers.values())
 
     def reset(epoch: int) -> None:
-        if reset_senders_interval is not None:
-            if epoch % reset_senders_interval == 0:
-                index = (epoch // reset_senders_interval) % len(senders)
-                senders[index].apply(init_weights)
+        if reset_senders_interval and epoch % reset_senders_interval == 0:
+            index = (epoch // reset_senders_interval) % len(senders)
+            for i in range(reset_num_senders_once):
+                senders[(index + i) % len(senders)].apply(init_weights)
 
-        if reset_receivers_interval is not None:
-            if epoch % reset_receivers_interval == 0:
-                index = (epoch // reset_receivers_interval) % len(receivers)
-                receivers[index].apply(init_weights)
+        if reset_receivers_interval and epoch % reset_receivers_interval == 0:
+            index = (epoch // reset_receivers_interval) % len(receivers)
+            for i in range(reset_num_receivers_once):
+                receivers[(index + i) % len(receivers)].apply(init_weights)
 
     train(
         game=game,
