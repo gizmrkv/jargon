a1cce12f25b21585b7fc284fda1d19003f42ad16
gizmrkv <gizmrkv@gmail.com>
2024-04-20 23:59:25 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jargon",
+    version="0.1.0",
+    url="https://github.com/gizmrkv/jargon",
+    author="gizmrkv",
+    author_email="gizmrkv@gmail.com",
+    packages=find_packages(),
+)
