a8925844c778617e7db902fabdb047355321135d
gizmrkv <gizmrkv@gmail.com>
2024-04-21 00:04:42 +0900

    Add autoencoder

---
+++ example/autoencoder/autoencoder.py
@@ -0,0 +1,114 @@
+from typing import Literal, Optional
+
+import torch
+from torch import nn, optim
+
+from jargon.callback import MetricsCallback
+from jargon.core import Trainer
+from jargon.envs import AutoencoderEnv
+from jargon.logger import DuplicateChecker, WandBLogger
+from jargon.loss import LossSelector, TextCrossEntropyLoss
+from jargon.net import RecurrentDecoder, RecurrentEncoder, RecurrentNet
+from jargon.stopper import MinMaxEarlyStopper
+from jargon.utils import fix_seed, init_weights
+from jargon.utils.torchdict import TensorDict
+
+
+def run_autoencoder(
+    epochs: int,
+    vocab_size: int,
+    seq_length: int,
+    batch_size: int,
+    encoder_embed_size: int,
+    encoder_hidden_size: int,
+    decoder_embed_size: int,
+    decoder_hidden_size: int,
+    encoder_num_layers: int = 1,
+    encoder_bias: bool = True,
+    encoder_dropout: float = 0.0,
+    encoder_bidirectional: bool = True,
+    encoder_rnn_type: Literal["rnn", "lstm", "gru"] = "lstm",
+    decoder_num_layers: int = 1,
+    decoder_bias: bool = True,
+    decoder_dropout: float = 0.0,
+    decoder_rnn_type: Literal["rnn", "lstm", "gru"] = "lstm",
+    decoder_train_temp: float = 1.0,
+    decoder_eval_temp: float = 1e-5,
+    decoder_peeky: bool = False,
+    lr: float = 1e-3,
+    seed: Optional[int] = None,
+):
+    if seed is not None:
+        fix_seed(seed)
+
+    rnn_types = {"rnn": nn.RNN, "lstm": nn.LSTM, "gru": nn.GRU}
+    encoder = RecurrentEncoder(
+        RecurrentNet(
+            encoder_embed_size,
+            encoder_hidden_size,
+            num_layers=encoder_num_layers,
+            bias=encoder_bias,
+            dropout=encoder_dropout,
+            bidirectional=encoder_bidirectional,
+            rnn_type=rnn_types[encoder_rnn_type],
+        ),
+        vocab_size=vocab_size,
+        embed_size=encoder_embed_size,
+    )
+    decoder = RecurrentDecoder(
+        RecurrentNet(
+            decoder_embed_size,
+            decoder_hidden_size,
+            num_layers=decoder_num_layers,
+            bias=decoder_bias,
+            dropout=decoder_dropout,
+            rnn_type=rnn_types[decoder_rnn_type],
+        ),
+        vocab_size=vocab_size,
+        embed_size=decoder_embed_size,
+        seq_length=seq_length,
+        train_temp=decoder_train_temp,
+        eval_temp=decoder_eval_temp,
+        peeky=decoder_peeky,
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    env = AutoencoderEnv(
+        encoder, decoder, vocab_size, seq_length, batch_size, device=device
+    ).to(device)
+    env.apply(init_weights)
+    loss_selector = LossSelector(decoder=TextCrossEntropyLoss(vocab_size, seq_length))
+    optimizer = optim.AdamW(env.parameters(), lr=lr)
+
+    def metrics(batches: TensorDict) -> TensorDict:
+        input = batches.get_tensor_dict("encoder")
+        input = input.get_tensor("input")
+        output = batches.get_tensor_dict("decoder")
+        output = output.get_tensor("output")
+        acc = (input == output).float().mean()
+        return TensorDict(acc=acc)
+
+    loggers = [WandBLogger(project="jargon"), DuplicateChecker()]
+    callbacks = {"metrics": MetricsCallback(metrics, loggers)}
+    stopper = MinMaxEarlyStopper(0.001, 10)
+    trainer = Trainer(
+        env,
+        loss_selector,
+        optimizer,
+        loggers=loggers,
+        callbacks=callbacks,
+        stopper=stopper,
+    )
+    trainer.train(epochs)
+
+
+if __name__ == "__main__":
+    run_autoencoder(
+        epochs=10000,
+        vocab_size=20,
+        seq_length=2,
+        batch_size=1024,
+        encoder_embed_size=32,
+        encoder_hidden_size=64,
+        decoder_embed_size=32,
+        decoder_hidden_size=64,
+    )
---
+++ jargon/core/trainer.py
@@ -0,0 +1,148 @@
+import datetime
+import os
+import random
+from pathlib import Path
+from typing import Dict, List, Optional
+
+import numpy as np
+import torch
+from torch import nn, optim
+from tqdm import tqdm
+
+from jargon.callback import Callback
+from jargon.envs import Environment
+from jargon.logger import Logger
+from jargon.loss import LossSelector
+from jargon.stopper import DummyEarlyStopper, EarlyStopper
+
+
+class Trainer:
+    def __init__(
+        self,
+        env: Environment,
+        loss_selector: LossSelector,
+        optimizer: optim.Optimizer,
+        loggers: Optional[List[Logger]] = None,
+        stopper: Optional[EarlyStopper] = None,
+        callbacks: Optional[Dict[str, Callback]] = None,
+        checkpoint_interval: Optional[int] = None,
+        checkpoint_path: Optional[Path] = None,
+        loss_max_norm: float = 0.5,
+        use_tqdm: bool = True,
+    ):
+        self.env = env
+        self.loss_selector = loss_selector
+        self.optimizer = optimizer
+        self.loggers = loggers or []
+        self.stopper = stopper or DummyEarlyStopper()
+        self.callbacks = callbacks or {}
+        self.checkpoint_interval = checkpoint_interval
+        self.checkpoint_path = checkpoint_path
+        self.loss_max_norm = loss_max_norm
+        self.use_tqdm = use_tqdm
+
+        assert checkpoint_interval is None or checkpoint_interval >= 1
+
+        dt = datetime.datetime.now()
+        self.run_name = dt.strftime("%Y-%m-%d %H-%M-%S-%f")
+        self.log_dir = Path(f"logs/{self.run_name}")
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        if checkpoint_path is not None:
+            self.load_checkpoint(checkpoint_path)
+
+    def train(self, epochs: int):
+        if epochs % 10 == 0:
+            epochs += 1
+
+        for logger in self.loggers:
+            logger.begin(self.run_name)
+
+        train_begin_log_dir = self.log_dir / "begin"
+        for name, callback in self.callbacks.items():
+            callback.on_train_begin(train_begin_log_dir / name)
+
+        zfill_width = len(str(epochs))
+        with tqdm(range(epochs), disable=not self.use_tqdm) as pbar:
+            for epoch in pbar:
+                epoch_log_dir = self.log_dir / str(epoch).zfill(zfill_width)
+                for name, callback in self.callbacks.items():
+                    callback.on_epoch_begin(epoch, epoch_log_dir / name)
+
+                batches_iter = self.env.rollout()
+                loss_list = []
+                while True:
+                    for callback in self.callbacks.values():
+                        callback.on_batch_begin(epoch)
+
+                    try:
+                        batches = next(batches_iter)
+                    except StopIteration:
+                        break
+
+                    self.env.train()
+                    losses = self.loss_selector.forward(batches)
+                    losses = losses.apply(lambda l: l.mean())
+                    loss = torch.stack([l for l in losses.values()]).sum()
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    nn.utils.clip_grad_norm_(self.env.parameters(), self.loss_max_norm)
+                    self.optimizer.step()
+
+                    loss_list.append(loss.item())
+
+                    for callback in self.callbacks.values():
+                        callback.on_batch_end(epoch, batches, losses)
+
+                for name, callback in self.callbacks.items():
+                    callback.on_epoch_end(epoch, epoch_log_dir / name)
+
+                for logger in self.loggers:
+                    logger.flush()
+
+                if (
+                    self.checkpoint_interval is not None
+                    and epoch % self.checkpoint_interval == 0
+                ):
+                    self.save_checkpoint(epoch_log_dir / "checkpoint.bin")
+
+                if self.stopper.step(sum(loss_list) / len(loss_list)):
+                    for callback in self.callbacks.values():
+                        callback.on_early_end(epoch)
+
+                    break
+
+        train_end_log_dir = self.log_dir / "end"
+        for name, callback in self.callbacks.items():
+            callback.on_train_end(train_end_log_dir / name)
+
+        for logger in self.loggers:
+            logger.close()
+
+    def save_checkpoint(self, checkpoint_path: Path):
+        checkpoint = {
+            "env": self.env.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+            # "scheduler": scheduler.state_dict(),
+            # "amp": amp.state_dict(), # apex混合精度を使用する場合は必要
+            "random": random.getstate(),
+            "np_random": np.random.get_state(),  # numpy.randomを使用する場合は必要
+            "torch": torch.get_rng_state(),
+            "torch_random": torch.random.get_rng_state(),
+            "cuda_random": torch.cuda.get_rng_state(),  # gpuを使用する場合は必要
+            "cuda_random_all": torch.cuda.get_rng_state_all(),  # 複数gpuを使用する場合は必要
+        }
+        torch.save(checkpoint, checkpoint_path)
+
+    def load_checkpoint(self, checkpoint_path: Path):
+        checkpoint = torch.load(checkpoint_path)
+        self.env.load_state_dict(checkpoint["env"])
+        self.optimizer.load_state_dict(checkpoint["optimizer"])
+        # scheduler.load_state_dict(checkpoint["scheduler"])
+        # amp.load_state_dict(checkpoint["amp"])
+        random.setstate(checkpoint["random"])
+        np.random.set_state(checkpoint["np_random"])
+        torch.set_rng_state(checkpoint["torch"])
+        torch.random.set_rng_state(checkpoint["torch_random"])
+        torch.cuda.set_rng_state(checkpoint["cuda_random"])
+        torch.cuda.torch.cuda.set_rng_state_all(checkpoint["cuda_random_all"])
